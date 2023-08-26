83c48dc7a127576f79cd2ac6e1f73571ae162e9d
Rikuto13ten <rikuto1367@gmail.com>
2023-08-26 18:22:02 +0900

    feat: pinCodeを実装

--- app/src/main/java/com/example/pincodeview/MainActivity.kt
+++ app/src/main/java/com/example/pincodeview/MainActivity.kt
@@ -1,58 +1,61 @@
 package com.example.pincodeview
 
+import android.content.Context
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
 import com.example.pincodeview.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        setSupportActionBar(binding.toolbar)
+        listOf(
+            binding.pinCodeTextView1,
+            binding.pinCodeTextView2,
+            binding.pinCodeTextView3,
+            binding.pinCodeTextView4
+        ).also {
+            it.forEachIndexed { index, textView ->
+                textView.setOnClickListener {
+                    binding.pinCodeEditText.requestFocus()
+                    val inputMethodManager =
+                        getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                    inputMethodManager.showSoftInput(
+                        binding.pinCodeEditText,
+                        InputMethodManager.SHOW_IMPLICIT
+                    )
+                }
+            }
+        }
 
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
+        binding.pinCodeEditText.addTextChangedListener(onTextWatcher(binding))
+    }
 
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+    private fun onTextWatcher(binding: ActivityMainBinding) = object : TextWatcher {
+        override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) = Unit
+        override fun afterTextChanged(p0: Editable?) = Unit
+
+        override fun onTextChanged(s: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            s?.let { text ->
+                binding.pinCodeTextView1.text = text.getString(0)
+                binding.pinCodeTextView2.text = text.getString(1)
+                binding.pinCodeTextView3.text = text.getString(2)
+                binding.pinCodeTextView4.text = text.getString(3)
+            }
         }
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
+    private fun CharSequence.getString(index: Int) = if (this.length > index) {
+        this[index].toString()
+    } else {
+        " "
     }
 }
