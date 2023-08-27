0fb1e502fb4d29ab78b196249d50623b2d97b8b8
Rikuto13ten <rikuto1367@gmail.com>
2023-08-27 17:52:24 +0900

    ref: kotlin Docを追加

--- app/src/main/java/com/example/pincodeview/MainActivity.kt
+++ app/src/main/java/com/example/pincodeview/MainActivity.kt
@@ -40,6 +40,14 @@ class MainActivity : AppCompatActivity() {
         binding.pinCodeEditText.addTextChangedListener(onTextWatcher(textViews))
     }
 
+    /**
+     * テキストの変更を監視し、それに応じて複数のTextViewのテキストを更新するTextWatcherを返します。
+     * CharSequenceの各文字は、提供されたTextViewリストの対応する位置に設定されます。
+     * 例: CharSequenceの最初の文字は、TextViewリストの最初の項目に設定されます。
+     *
+     * @param textViews 更新するTextViewのリスト。
+     * @return TextWatcher テキストの変更を監視するオブジェクト。
+     */
     private fun onTextWatcher(textViews: List<TextView>) = object : TextWatcher {
         override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) = Unit
         override fun afterTextChanged(p0: Editable?) = Unit
@@ -53,12 +61,25 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    /**
+     * 指定されたインデックスの文字を取得します。
+     * インデックスがCharSequenceの長さを超えている場合、空の文字列を返します。
+     *
+     * @param index 取得したい文字のインデックス。
+     * @return String 指定されたインデックスの文字。インデックスが範囲外の場合は空の文字列。
+     */
     private fun CharSequence.getString(index: Int) = if (this.length > index) {
         this[index].toString()
     } else {
         ""
     }
 
+    /**
+     * キーボードを表示します。
+     *
+     * @receiver View キーボードを表示するためのビュー。
+     * @return Boolean キーボードが正常に表示された場合はtrue、それ以外の場合はfalse。
+     */
     private fun View.showKeyboard(): Boolean {
         val inputMethodManager =
             getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
